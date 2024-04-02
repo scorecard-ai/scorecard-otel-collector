d0a59689ba9e2d4dc49fc1c61db441f1d231136f
Nathan Hammond <nathanhammond@getscorecard.ai>
2024-04-02 21:10:02 +0800

    Prepare for running from GitHub Actions.

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@ set -e
 
 rm -f ocb
 
+# Configure which environment we're building in.
+OS=darwin
+ARCH=arm64
+
 OCB_VERSION=0.97.0
-OCB_URL=`curl https://api.github.com/repos/open-telemetry/opentelemetry-collector/releases | jq -r ".[] | select(.tag_name==\"cmd/builder/v${OCB_VERSION}\") | .assets[] | select(.name==\"ocb_${OCB_VERSION}_darwin_arm64\") | .browser_download_url"`
+OCB_URL=`curl https://api.github.com/repos/open-telemetry/opentelemetry-collector/releases | jq -r ".[] | select(.tag_name==\"cmd/builder/v${OCB_VERSION}\") | .assets[] | select(.name==\"ocb_${OCB_VERSION}_${OS}_${ARCH}\") | .browser_download_url"`
 
 curl -L -o ocb $OCB_URL
 chmod +x ocb
