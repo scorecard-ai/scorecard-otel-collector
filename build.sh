c00aa1a9cf323863f04d0eefa9bd929db5228fc0
Nathan Hammond <nathanhammond@getscorecard.ai>
2024-04-02 21:05:03 +0800

    Update to ecosystem state.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 
 rm -f ocb
 
-OCB_VERSION=0.96.0
+OCB_VERSION=0.97.0
 OCB_URL=`curl https://api.github.com/repos/open-telemetry/opentelemetry-collector/releases | jq -r ".[] | select(.tag_name==\"cmd/builder/v${OCB_VERSION}\") | .assets[] | select(.name==\"ocb_${OCB_VERSION}_darwin_arm64\") | .browser_download_url"`
 
 curl -L -o ocb $OCB_URL
